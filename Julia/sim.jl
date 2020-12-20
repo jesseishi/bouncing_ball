5374eef0d1481320ea01365e51e753190c7e7753
Jesse Hummel <jesseishistorm@gmail.com>
2020-12-20 16:36:49 +0100

    --amend

--- Julia/sim.jl
+++ Julia/sim.jl
@@ -3,7 +3,6 @@
 # Start with how Kalman filters work.
 
 # Usings.
-using DataFrames
 using HDF5
 using CSV
 
@@ -50,15 +49,6 @@ function main()
     n_continuous_steps = floor(Int, (t1-t0) / dt)
 
     # Set up data logging.
-    ball_data = DataFrame(t = Float64[], x = Float64[], y = Float64[])
-    push!(ball_data, [t0 ball_state_k.pos[1] ball_state_k.pos[2]])
-
-    sensor_data = DataFrame(t = Float64[], x = Float64[], y = Float64[])
-    push!(sensor_data, [t0 pos_star[1] pos_star[2]])
-
-    filter_data = DataFrame(t = Float64[], x = Float64[], y = Float64[])
-    push!(filter_data, [t0 pos_hat[1] pos_hat[2]])
-
     # The time dimension is N+1 because we also want to store the value before the first step.
     ball_data2 = Array{Float64, 2}(undef, n_continuous_steps+1, 2)
     ball_data2[1, :] = ball_state_k.pos
@@ -91,7 +81,6 @@ function main()
         ball_state_t = ball_state_km1
         for t in t_km1+dt : dt : t_km1+Δt
             ball_state_t = Ball.step(ball_state_t, ball_params, dt)
-            push!(ball_data, [t ball_state_t.pos[1] ball_state_t.pos[2]])
             ball_data2[round(Int, t/dt)+1, :] = ball_state_t.pos
         end
         ball_state_k = ball_state_t  # TODO: This is kinda awkward, maybe just get rid of the whole k and k-1 thing?
@@ -102,7 +91,6 @@ function main()
 
         # The sensor.
         pos_star = Sensor.measure(ball_state_k.pos, sensor_params)
-        push!(sensor_data, [t_k pos_star[1] pos_star[2]])
         sensor_data2[i_k, :] = pos_star
 
         # The particle filter.
@@ -116,10 +104,6 @@ function main()
 
     # Saving the data.
     # Not sure why we should also specify the Julia folder here since this file is inside the Julia folder...
-    CSV.write("Julia/results/data/ball_data", ball_data)
-    CSV.write("Julia/results/data/sensor_data", sensor_data)
-    CSV.write("Julia/results/data/filter_data", filter_data)
-
     h5open("Julia/results/data/results.h5", "w") do fid
         g_continuous = create_group(fid, "continuous")
         g_continuous["t"] = collect(t0:dt:t1)
